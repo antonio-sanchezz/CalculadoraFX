bd27bd31d2b2592f7abc38f69f4beba13172552d
Antonio Sanchez Espinosa <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-03-17 22:11:35 +0100

    Initial commit

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -13,9 +13,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-    	// Cargar ventana modal.
-        //Parent root = FXMLLoader.load(getClass().getResource("viewsandcontrollers/main/Main.fxml"));
-    	// Cargar calculadora.
     	Parent root = FXMLLoader.load(getClass().getResource("viewsandcontrollers/calculadora/Calculadora.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
